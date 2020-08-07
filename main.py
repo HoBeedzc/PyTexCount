46f36d0927a12d5f2ae402c7347b278cbd22aef6
HoBeedzc <lzqpublic@163.com>
2020-08-07 12:28:48 +0800

    add some feature

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 # About
+'''
+Author : Hobee
+Date : 2020-08-07
+Version : 1.0.0
+contact me at : me@hobeedzc.cn
+more info : https://github.com/HoBeedzc/PyTexCount
+(c) copyright 1999-2020 Hobee. All rights reserved.
+'''
 
 # Third party libraries
 import re
+import sys
 
 # global variables
 Chinese_punctuation = '''  '''
 English_punctuation = '''  '''
 Alpha_string = '''ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'''
 Digit_string = '''1234567890'''
+White_space = ''' \n\r\f\t\v'''
 
 
 def read_tex(file):
@@ -15,16 +25,58 @@ def read_tex(file):
     # return a string and ignore characters before '\begin{document}'
     with open(file, 'r', encoding='utf-8') as f:
         tex = f.read()
-        index = re.search('begin{document}', tex, flags=0).span()
-        return tex[:index[1]], tex[index[1]:]
+    return tex
+
+
+def extract_body(tex):
+    #
+    #
+    begin_index = re.search(r'\\begin{document}', tex, flags=0).span()
+    end_index = re.search(r'\\end{document}', tex, flags=0).span()
+    header = tex[:begin_index[0]]
+    body = tex[begin_index[0]:end_index[1]]
+    footer = tex[end_index[1]:]
+    return header, body, footer
+
+
+def remove_whitespace(doc):
+    #
+    #
+    redoc = ''
+    for i in doc:
+        if i not in White_space or i == '\n':
+            redoc += i
+    return redoc
+
+
+'''
+def process_header(header):
+    #
+    # 
+    return
+
+
+def process_body(body):
+    #
+    # 去掉制表符和空格
+    body = body.replace('\t','').replace(' ','')
+    return body
+
+
+def process_footer(footer):
+    #
+    #
+    return
+'''
 
 
 def count_(tex):
     # input a string which read_tex() return
     # return a int that the number of Chinese characters it includes
     nex_tex = ''
+    '''
     for i in tex:
-        if i in '!@#$%^&*(){}[]|\\;:",<.>/?\n-_=+；：’“，。？！（）【】、 ':
+        if i in '!@#$%^&*(){}[]|\\;:",<.>/?\n-_=+；：’“，。？！（）【】、 \t':
             pass
         elif i in Alpha_string:
             pass
@@ -32,6 +84,12 @@ def count_(tex):
             pass
         else:
             nex_tex += i
+    '''
+    f = open(r'testfile/content.txt', 'w')
+    for i in tex:
+        f.write(i)
+        # f.write('\n')
+    f.close()
     return len(nex_tex)
 
 
@@ -40,17 +98,26 @@ def count_packages(tex_pre):
     #
     packages = []
     n = 0
-    used_packages = re.findall('usepackage{[\S]*}', pre_tex)
+    used_packages = re.findall(r'\\usepackage\[*.*\]*{.*}', tex_pre)
     for i in used_packages:
-        packages.append(i[11:-1])
+        packages.append(re.findall(r'{.*}', i)[0][1:-1])
         n += 1
     return packages, n
 
 
-if __name__ == '__main__':
-    file_name = input('Enter a tex file path...')
-    pre_tex, tex = read_tex(file_name)
+def main():
+    # file_name = input('Enter a tex file path...')
+    file_name = r'testfile/paper.tex'
+    tex = read_tex(file_name)
+    header, body, footer = extract_body(tex)
+    header, body, footer = map(remove_whitespace, [header, body, footer])
     print('count:', end=' ')
-    print(count_(tex))
-    print('used_packages:', end='')
-    print(count_packages(pre_tex))
+    print(count_(body))
+    print('used packages:', end=' ')
+    print(count_packages(header))
+
+
+if __name__ == '__main__':
+    argvs = sys.argv
+    print(argvs)
+    main()
